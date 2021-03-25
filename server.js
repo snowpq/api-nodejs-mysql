f93249ec21fe20aeede9032358ffeb4f0f7a833b
Pablo <snow.cod@gmail.com>
2021-03-25 15:44:40 -0300

    Change port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 9000)
 const dbOptions = {
     host: 'localhost',
     port: 3306,
@@ -27,3 +27,5 @@ app.use('/api', routes)
 app.listen(app.get('port'), ()=>{
     console.log('server running on port', app.get('port'))
 })
+
+
